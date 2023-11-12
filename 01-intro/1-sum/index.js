31976bdb84316d7a18098d08536294b12769efcd
numowork <numowork@gmail.com>
2023-11-12 23:19:15 +0300

    Revert "1st task"
    This reverts commit 219f62a5c0f0db695c98f766c2c93b13a62c3d84.

--- 01-intro/1-sum/index.js
+++ 01-intro/1-sum/index.js
@@ -5,12 +5,6 @@
  * @param {number} n second number
  * @returns {number}
  */
-const sum = (...args) => {
-  for (let num of args) {
-    if (!Number(num)) {
-      throw Error('INVALID_ARGUMENT_TYPE')
-    }
-  }
-  const initialValue = 0;
-  return args.reduce((acc, cur) => acc + cur, initialValue);
+export const sum = (m, n) => {
+
 };
