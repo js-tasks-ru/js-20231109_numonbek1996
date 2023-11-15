f47934ee801ef7377e1ab83e7b3e0ab3d1648445
numowork <numowork@gmail.com>
2023-11-15 22:56:03 +0300

    update 2-2st task and finish 2-3st task

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -4,17 +4,18 @@
  * @param {...string} fields - the properties paths to pick
  * @returns {object} - returns the new object
  */
-const pick = (obj, ...fields) => {
+export const pick = (obj, ...fields) => {
   if (typeof obj !== 'object' || Object.keys(obj).length < 1) {
     throw new Error('INVALID_OBJECT')
   }
 
   const basket = {}
-  // for of faster than other Array methods
-  for (let filed of fields) {
-    if (!obj.hasOwnProperty(filed)) continue
 
-    Object.assign(basket, {[filed]: obj[filed]})
+  for (const [key, value] of Object.entries(obj)) {
+    if (!fields.includes(key)) continue
+
+    basket[key] = value
   }
+
   return basket
 };
--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -4,6 +4,29 @@
  * @param {...string} fields - the properties paths to omit
  * @returns {object} - returns the new object
  */
-export const omit = (obj, ...fields) => {
+const omit = (obj, ...fields) => {
+  if (typeof obj !== 'object' || Object.keys(obj).length < 1) {
+    throw new Error('INVALID_OBJECT')
+  }
 
+  const not_listed = {...obj}
+  fields.forEach(el => delete not_listed[el])
+  return not_listed
 };
+
+// 2
+// const omit = (obj, ...fields) => {
+//   if (typeof obj !== 'object' || Object.keys(obj).length < 1) {
+//     throw new Error('INVALID_OBJECT')
+//   }
+//
+//   const not_listed = {}
+//
+//   for (const [key, value] of Object.entries(obj)) {
+//     if (fields.includes(key)) continue
+//
+//     not_listed[key] = obj[key]
+//   }
+//
+//   return not_listed
+// };
