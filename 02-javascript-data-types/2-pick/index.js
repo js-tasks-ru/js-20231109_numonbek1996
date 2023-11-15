88392db95a6a9c3864119b9a93e177e1f5d0a40c
numowork <numowork@gmail.com>
2023-11-15 22:31:12 +0300

    2-2st task

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -4,6 +4,17 @@
  * @param {...string} fields - the properties paths to pick
  * @returns {object} - returns the new object
  */
-export const pick = (obj, ...fields) => {
+const pick = (obj, ...fields) => {
+  if (typeof obj !== 'object' || Object.keys(obj).length < 1) {
+    throw new Error('INVALID_OBJECT')
+  }
 
+  const basket = {}
+  // for of faster than other Array methods
+  for (let filed of fields) {
+    if (!obj.hasOwnProperty(filed)) continue
+
+    Object.assign(basket, {[filed]: obj[filed]})
+  }
+  return basket
 };
