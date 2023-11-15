a0642904dc07eb42af152684d33220bcc0c264a8
numowork <numowork@gmail.com>
2023-11-15 22:03:53 +0300

    2st task

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,15 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
+function sortStrings(arr, param = 'asc') {
+  const sortBy = {
+      asc: -1,
+      desc: 1
+    },
+    locale = ['ru', 'en'],
+    options = {caseFirst: 'upper'}
 
+  return arr.sort((a, b) =>
+    (sortBy[param] || 1) * a.localeCompare(b, locale, options)
+  )
 }
