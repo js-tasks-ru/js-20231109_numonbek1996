d32d484904850446f159d806d4ecd4a536e471bb
numowork <numowork@gmail.com>
2023-11-19 19:42:32 +0300

    3-2st task done

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,7 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
+  if (!obj || typeof obj !== 'object') return undefined
 
+  return Object.fromEntries(Object.entries(obj).map(a => a.reverse()))
 }
